70e59900c4bff772fc8acf8307c2968e25456251
MVasquezR <josemartivr@gmail.com>
2021-03-03 09:40:02 -0500

    .

--- prueba.py
+++ prueba.py
@@ -1,12 +1,15 @@
-from core.auth_methods.ssh import PublicKeyAuthMethod
+from core.auth_methods.ssh import UserPasswordAuthMethod
 from core.connectors import SshConnector
 from core.scripts.simple_flow import SimpleScript
 
 
 class TestScript(SimpleScript):
     CONNECTOR_CLASS = SshConnector
-    AUTH_METHOD_CLASS = PublicKeyAuthMethod
+    AUTH_METHOD_CLASS = UserPasswordAuthMethod
     SCRIPT_PARAMS_DICT = {}
+
+    def validate(self):
+        return True
 
     def run(self):
         print('ingreso')
