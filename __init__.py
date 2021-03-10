ae3eabbfe45841f129b4ea22f47fe004d8c2e525
MVasquezR <josemartivr@gmail.com>
2021-03-10 10:08:38 -0500

    .

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from display_interface_brief import DisplayInterfaceBrief
+from display_interface_brief_2 import DisplayInterfaceBriefB
 
 SCRIPTS = {
-    'display_interface_brief': DisplayInterfaceBrief
+    'display_interface_brief': DisplayInterfaceBrief,
+    'display_interface_brief_b': DisplayInterfaceBriefB
 }
