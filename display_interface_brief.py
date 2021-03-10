2516fa64cadb3fe7aa5311bd096d47472c9864a4
MVasquezR <josemartivr@gmail.com>
2021-03-10 12:55:22 -0500

    .

--- display_interface_brief.py
+++ display_interface_brief.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from core import task_status
 from core.auth_methods.ssh import UserPasswordAuthMethod
 from core.commands.ssh import NonUnixCommand
 from core.connectors import SshConnector
@@ -22,36 +23,42 @@ class DisplayInterfaceBrief(SimpleScript):
         return None
 
     def run(self):
-        ls_result_scripts = []
-        for commd in ['screen-length 0 temporary', 'display interface brief']:
-            class TempCmd(NonUnixCommand):
-                DEFAULT_RESPONSE = "\n".join([commd])
+        try:
+            ls_result_scripts = []
+            for commd in ['screen-length 0 temporary', 'display interface brief']:
+                class TempCmd(NonUnixCommand):
+                    DEFAULT_RESPONSE = "\n".join([commd])
 
-                def __init__(
-                        self,
-                        timeout: Optional[int] = None
-                ):
-                    super().__init__(
-                        cmd=self.DEFAULT_RESPONSE,
-                        timeout=timeout,
-                        wait_response=True
-                    )
+                    def __init__(
+                            self,
+                            timeout: Optional[int] = None
+                    ):
+                        super().__init__(
+                            cmd=self.DEFAULT_RESPONSE,
+                            timeout=timeout,
+                            wait_response=True
+                        )
 
-                @property
-                def is_success(self) -> bool:
-                    return True
+                    @property
+                    def is_success(self) -> bool:
+                        return True
 
-            ls_result_scripts.append(self._connection.execute_command(TempCmd()))
+                ls_result_scripts.append(self._connection.execute_command(TempCmd()))
 
-        self._result = ls_result_scripts[1]
-        ls_result = self._result.split('\r\n')
-        indice_next = [index for index, row in enumerate(ls_result) if 'inErrors' in row]
-        for index, row in enumerate(ls_result):
-            if index > indice_next[0]:
-                len_row = len(row)
-                row_to_validate = row[len_row - 12:len_row]
-                if row_to_validate[0] != '0' and row_to_validate[len(row_to_validate) - 1] != '0':
-                    if '>' not in row:
-                        self.set_user_log(row)
-
+            self._result = ls_result_scripts[1]
+            ls_result = self._result.split('\r\n')
+            indice_next = [index for index, row in enumerate(ls_result) if 'inErrors' in row]
+            for index, row in enumerate(ls_result):
+                if index > indice_next[0]:
+                    len_row = len(row)
+                    row_to_validate = row[len_row - 12:len_row]
+                    if row_to_validate[0] != '0' or row_to_validate[len(row_to_validate) - 1] != '0':
+                        if '>' not in row:
+                            self.set_user_log(row)
+            if len(self._user_log) != 0:
+                self.status = task_status.WARNING
+            else:
+                self.status = task_status.SUCCESS
+        except Exception as e:
+            self.status = task_status.FAIL
         return True
